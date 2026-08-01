28e4b2e42b6fe7b322868a215e6dda9682d25c18
IlhamAgusk <ilhamagusk@gmail.com>
2026-08-01 19:27:14 +0700

    CRUD User dan Atraksi serta Validasi Form 1

---
+++ database/seeders/DestinasiSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Destinasi;
+
+class DestinasiSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Destinasi::truncate();
+
+        Destinasi::create([
+            'nama' => 'Istana Siak',
+            'deskripsi' => 'Istana peninggalan Kesultanan Siak Sri Indrapura, dibangun pada masa kejayaan kesultanan Melayu di tepian Sungai Siak. Kini menjadi museum yang menyimpan berbagai koleksi benda bersejarah dan menjadi ikon utama wisata sejarah Kabupaten Siak.',
+            'gambar' => 'istana-siak.jpg',
+            'jam_buka' => '08:00:00',
+            'jam_tutup' => '17:00:00',
+            'lokasi' => 'Kecamatan Siak, Kabupaten Siak',
+        ]);
+
+        Destinasi::create([
+            'nama' => 'Tangsi Belanda',
+            'deskripsi' => 'Bangunan barak militer peninggalan zaman kolonial Belanda yang dulunya berfungsi sebagai zona pertahanan tentara Belanda. Setelah dipugar, kini menjadi destinasi wisata edukasi sejarah dengan bangunan yang masih terjaga bentuk aslinya, lengkap dengan ruang-ruang yang dulu dipakai sebagai barak, gudang senjata, dan ruang komando.',
+            'gambar' => 'tangsi-belanda.jpeg',
+            'jam_buka' => '08:00:00',
+            'jam_tutup' => '17:00:00',
+            'lokasi' => 'Kampung Benteng Hulu, Kecamatan Mempura, Kabupaten Siak',
+        ]);
+
+        Destinasi::create([
+            'nama' => 'Skywalk Tengku Buwang Asmara',
+            'deskripsi' => 'Jembatan layang pejalan kaki sepanjang lebih dari 1 kilometer yang membentang di atas Sungai Siak, dengan sebagian lantai kaca tempered transparan setinggi sekitar 12 meter dari permukaan air. Dinamai dari Sultan Siak kedua, jembatan ini menjadi spot favorit menikmati pemandangan Istana Siak dan senja, apalagi pada malam hari dengan lampu hias yang menari.',
+            'gambar' => 'skywalk-tengku-buwang-asmara-siak.jpg',
+            'jam_buka' => '16:00:00',
+            'jam_tutup' => '22:00:00',
+            'lokasi' => 'Kampung Tengah, Kecamatan Mempura, Kabupaten Siak',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DestinasiController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AtraksiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +31,22 @@ Route::delete('/destinasi/{id}', [DestinasiController::class, 'destroy'])->name(
 
 // Route detail (pakai {id}, WAJIB paling bawah di antara route destinasi)
 Route::get('/destinasi/{id}', [DestinasiController::class, 'show'])->name('destinasi.detail');
+
+// Route CRUD User
+Route::get('/user', [UserController::class, 'index'])->name('user');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+// Route CRUD Atraksi
+Route::get('/atraksi', [AtraksiController::class, 'index'])->name('atraksi');
+Route::get('/atraksi/create', [AtraksiController::class, 'create'])->name('atraksi.create');
+Route::post('/atraksi', [AtraksiController::class, 'store'])->name('atraksi.store');
+Route::get('/atraksi/{id}/edit', [AtraksiController::class, 'edit'])->name('atraksi.edit');
+Route::put('/atraksi/{id}', [AtraksiController::class, 'update'])->name('atraksi.update');
+Route::delete('/atraksi/{id}', [AtraksiController::class, 'destroy'])->name('atraksi.destroy');
 
 Route::get('/tentang', function () {
     return view('tentang');
